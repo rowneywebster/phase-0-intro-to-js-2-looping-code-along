881f0665a0c91da24515c4751fb7afc27121ef96
Rowney Ouma <oumarowney@gmail.com>
2025-04-13 23:15:27 +0300

    All done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+  }
+  function countDown(num) {
+    while (num >= 0) {   
+        console.log(num);
+      num--;  
+    }
+  }
+  
+  
+  countDown(10);
+  
